765da5b6fae0582b182d9750d5ff445f6e9fc244
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-04-08 01:15:18 +0800

    add test framework support

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
 JS_SLANG="node node_modules/js-slang/dist/repl/repl.js"
+TEST_FRAMEWORK="src/test/framework/main.js"
 
 SOURCEFILES=src/*/*.js
+TEST_FRAMEWORK_FILE="source-test.js"
 
 red=`tput setaf 1`
 green=`tput setaf 2`
@@ -28,6 +30,30 @@ $DIFF"
     
 }
 
+# $1 is the Source test file which uses the test framework
+test_source_framework() {
+    # run concatenation of test framework and test file
+    RESULTS=$($JS_SLANG -e --chapter=4 "$(cat $TEST_FRAMEWORK $1)")
+    
+    # retrieve names for tests that passed
+    while read test_name
+    do 
+        passed=$(($passed+1))
+        echo "${green}PASS $1 $test_name"
+    done < <(echo ${RESULTS} | grep -o '\w* PASSED' | awk -F 'PASSED' '{print $1}')
+    
+    # retrieve names and error messages for tests that failed
+    while read test_info 
+    do 
+        failed=$(($failed+1))
+        echo $test_info | awk -F 'FAILED:' '{ print $1 ":" $2 }' | awk -F '"' '{ print $1 $2 }' | 
+            while read test_name test_error 
+            do echo "${red}FAIL $1 $test_name $test_error"; 
+            done
+    done < <(echo ${RESULTS} | grep -o '\w* FAILED:[^"]*')
+                           
+}
+
 main() {
     for s in ${SOURCEFILES}
     do
@@ -40,6 +66,13 @@ main() {
 	    do
 		test_source $s $i
 	    done
+
+        # check if test framework is being used
+        TEST_PATH="$DIR/__tests__/$TEST_FRAMEWORK_FILE"
+        if [ -e $TEST_PATH ]
+        then
+            test_source_framework $TEST_PATH
+        fi
 	fi
     done
 }
