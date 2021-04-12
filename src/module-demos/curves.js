42d802841ab0eba35bbbfc8065a1ee758de19311
yongxiangng <yongxiangng@gmail.com>
2021-04-12 19:30:19 +0800

    Update Curve related demos

---
+++ src/module-demos/curves.js
@@ -0,0 +1,110 @@
+// Curves: A "curves" library for drawing curves with functional programming
+
+// copy this program into the Source Academy playground
+// https://sourceacademy.nus.edu.sg/playground
+
+// instructions: uncomment one of the draw_* lines
+//               below and then press "Run"
+//               (if necessary, adjust the size of REPL tab)
+//               Click on the Curves Canvas tab spawned to view
+
+// For full documentation of CURVES, see 
+//                  https://sicp.comp.nus.edu.sg/source/CURVES/
+
+import {
+    make_3D_point,
+    make_color_point,
+    make_3D_color_point,
+    draw_connected_full_view,
+    draw_points_full_view_proportional,
+    draw_3D_connected_full_view,
+    draw_3D_points_full_view_proportional,
+    unit_circle,
+    connect_ends,
+    translate,
+    scale,
+    rotate_around_origin
+} from "curves";
+
+const colored_unit_circle = t => make_color_point(
+    0.5 * math_cos(2 * math_PI * t) + 0.5,             // x-coordinate
+    0.5 * math_sin(2 * math_PI * (t + 0.5)) + 0.5,     // y-coordinate
+    math_pow(math_cos(2 * math_PI * t), 2) * 255,      // r-component
+    math_pow(math_sin(2 * math_PI * t), 2) * 255,      // g-component
+    t * 255);                                          // b-component
+
+const spiral = t => make_3D_point(
+    0.5 * math_cos(2 * math_PI * t) + 0.5,           // x-component
+    0.5 * math_sin(2 * math_PI * (t + 0.5)) + 0.5,   // y-component
+    t);                                              // z-component
+
+const colored_spiral = t => make_3D_color_point(
+    0.5 * math_cos(2 * math_PI * t) + 0.5,           // x-component
+    0.5 * math_sin(2 * math_PI * (t + 0.5)) + 0.5,   // y-component
+    t,                                               // z-component
+    math_pow(math_cos(2 * math_PI * t), 2) * 255,    // r-component
+    math_pow(math_sin(2 * math_PI * t), 2) * 255,    // g-component
+    t * 255);                                        // b-component
+
+const colored_spring = rotate_around_origin(0, math_PI/4, 0)(translate(2, 2, -2)
+    (scale(7, 3, 2)
+    (connect_ends(colored_unit_circle,
+    connect_ends(colored_spiral, colored_unit_circle)))));
+
+const to_domain = x => (2 * x - 1) * 100 * math_PI; // map to right domain
+
+const heart_scale = (t, heart) => math_sin(math_PI * t) * heart; // x, y scaler
+
+const heart = t => make_3D_point(
+    t,
+    heart_scale(t, 16 * math_pow(math_sin(to_domain(t)), 3)),
+    heart_scale(t, 13 * math_cos(to_domain(t)) - 5 * math_cos(2 * to_domain(t)) 
+    - 2 * math_cos(3 * to_domain(t)) - math_cos(4 * to_domain(t))));
+
+const colored_heart = t => make_3D_color_point(
+    t,
+    heart_scale(t, 16 * math_pow(math_sin(to_domain(t)), 3)),
+    heart_scale(t, 13 * math_cos(to_domain(t)) - 5 * math_cos(2 * to_domain(t)) 
+    - 2 * math_cos(3 * to_domain(t)) - math_cos(4 * to_domain(t))),
+    math_pow(math_cos(2 * math_PI * t), 2) * 255,    // r-component
+    math_pow(math_sin(2 * math_PI * t), 2) * 255,    // g-component
+    t * 255);                                        // b-component
+
+// =============================================================================
+// 2D Curves
+//
+// All drawings below here show 2D Curves, colored and non colored.
+// =============================================================================
+
+// Example of a circle with draw connected and points functions
+// draw_connected_full_view(100)(unit_circle);
+// draw_points_full_view_proportional(100)(unit_circle);
+
+// Example of a colored circle with draw connected and points functions
+// draw_connected_full_view(100)(colored_unit_circle);
+// draw_points_full_view_proportional(100)(colored_unit_circle);
+
+
+
+// =============================================================================
+// 3D Curves
+//
+// All drawings below here show 3D Curves, colored and non colored.
+// =============================================================================
+
+// Example of spiral with draw connected and points functions
+// draw_3D_connected_full_view(100)(spiral);
+// draw_3D_points_full_view_proportional(100)(spiral);
+
+// Example of colored spiral with draw connected and points functions
+// draw_3D_connected_full_view(100)(colored_spiral);
+// draw_3D_points_full_view_proportional(100)(colored_spiral);
+
+// Example of colored spring with draw connected and points functions
+// draw_3D_connected_full_view(500)(colored_spring);
+// draw_3D_points_full_view_proportional(500)(colored_spring);
+
+// Example of 
+// draw_3D_connected_full_view(10000)(heart);
+draw_3D_connected_full_view(10000)(colored_heart);
+
--- src/module-demos/times.js
+++ src/module-demos/times.js
@@ -2,16 +2,22 @@
 
 // copy this program into the Source Academy playground
 // https://sourceacademy.nus.edu.sg/playground
-// and choose "Source §2" and "CURVES"
 
 // instructions: uncomment one of the draw_times_table lines
 //               below and then press "Run"
 //               (if necessary, adjust the size of REPL tab)
+//               Click on the Curves Canvas tab spawned to view
 
 // connect_in_circle(n, f) connects n positions on the unit
 // circle. Each dot results from applying f to position number.
 // For full documentation of CURVES, see 
 //                  https://sicp.comp.nus.edu.sg/source/CURVES/
+
+import {
+    draw_connected_full_view, 
+    unit_circle,
+    make_color_point
+} from "curves";
 
 const connect_in_circle =
     (n, f) =>
@@ -25,15 +31,39 @@ const draw_times_table =
                              // pattern: pos1-dest1-pos1-pos2-dest2...
                              return k % 3 === 1 ? m * v * 3 : v * 3; }
                      );
+                     
+const colored_unit_circle = t => make_color_point(
+    0.5 * math_cos(2 * math_PI * t) + 0.5,             // x-coordinate
+    0.5 * math_sin(2 * math_PI * (t + 0.5)) + 0.5,     // y-coordinate
+    math_pow(math_cos(2 * math_PI * t), 2) * 255,      // r-component
+    math_pow(math_sin(2 * math_PI * t), 2) * 255,      // g-component
+    t * 255);    
+  
+const connect_in_colored_circle =
+    (n, f) =>
+        draw_connected_full_view(n)
+        (t => colored_unit_circle(f(math_round(t * n)) / n));
+        
+const draw_colored_times_table =
+    (n, m) =>
+    connect_in_colored_circle(n * 3,
+                      k => { const v = math_round((k - 1) / 3);
+                             // pattern: pos1-dest1-pos1-pos2-dest2...
+                             return k % 3 === 1 ? m * v * 3 : v * 3; }
+                     );
 
+// =============================================================================
+// Cardoids
+//
+// for background on times tables and cardoids, see 
+// https://www.youtube.com/watch?v=qhbuKbxJsk8
+// =============================================================================        
 // example how connect_in_circle works: draw polygon with 11 points:
 // connect_in_circle(11, k => k);
 
 // draw star with 11 points:
 // connect_in_circle(11, k => 4 * k);
 
-// for background on times tables and cardoids, see 
-// https://www.youtube.com/watch?v=qhbuKbxJsk8
 
 // draw_times_table(100, 2);      // m = 2: cardioid: 1 lobe
 // draw_times_table(100, 3);      // m = 3: nephroid: 2 lobes
@@ -47,10 +77,42 @@ const draw_times_table =
 // draw_times_table(295, 100);    // m = (n + 5) / 3: 3-layer nephroid
 // draw_times_table(594, 200);    // m = (n + 6) / 3: 3-layer 3 lobes...
 
- draw_times_table(395, 100);    // m = (n + 5) / 4: 4-layer cardioid
+draw_times_table(395, 100);    // m = (n + 5) / 4: 4-layer cardioid
 // draw_times_table(494, 100);    // m = (n + 6) / 5: 5-layer cardioid
 // draw_times_table(593, 100);    // m = (n + 7) / 6: 6-layer cardioid...
 
 // also nice:
 // draw_times_table(400, 201);    // m = n / 2 + 1 (/4,/8,/16)
 // draw_times_table(200, 99);     // m = (n / 2) - 1: square pattern
+
+// =============================================================================
+// Colored Cardoids
+//
+// Extension of Cardoids
+// =============================================================================
+
+// example how connect_in_colored_circle works: draw polygon with 11 points:
+// connect_in_colored_circle(11, k => k);
+
+// draw star with 11 points:
+// connect_in_colored_circle(11, k => 4 * k);
+
+// draw_colored_times_table(100, 2);      // m = 2: cardioid: 1 lobe
+// draw_colored_times_table(100, 3);      // m = 3: nephroid: 2 lobes
+// draw_colored_times_table(100, 4);      // m = 4: 3 lobes...
+
+// draw_colored_times_table(397, 200);    // m = (n + 3) / 2: 2-layer cardioid
+// draw_colored_times_table(500, 252);    // m = (n + 4) / 2: 2-layer nephroid
+// draw_colored_times_table(501, 253);    // m = (n + 5) / 2: 3-layer 3 lobes...
+
+// draw_colored_times_table(500, 168);    // m = (n + 4) / 3: 3-layer cardioid
+// draw_colored_times_table(295, 100);    // m = (n + 5) / 3: 3-layer nephroid
+// draw_colored_times_table(594, 200);    // m = (n + 6) / 3: 3-layer 3 lobes...
+
+// draw_colored_times_table(395, 100);    // m = (n + 5) / 4: 4-layer cardioid
+// draw_colored_times_table(494, 100);    // m = (n + 6) / 5: 5-layer cardioid
+// draw_colored_times_table(593, 100);    // m = (n + 7) / 6: 6-layer cardioid...
+
+// also nice:
+// draw_colored_times_table(400, 201);    // m = n / 2 + 1 (/4,/8,/16)
+// draw_colored_times_table(200, 99);     // m = (n / 2) - 1: square pattern
